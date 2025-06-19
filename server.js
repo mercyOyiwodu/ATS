efc801f2e07c005f2cd100acbab612c7a257c35c
muabiodun99 <Muabiodun101@gmail.com>
2025-06-19 20:01:48 +0100

    refactor: update bcrypt dependency and add nodemon for development

--- server.js
+++ server.js
@@ -1,22 +1,15 @@
 require('dotenv').config();
 const express = require("express");
-const connectDB = require('./config/database');
+require('./config/database');
 const authRoutes = require('./routes/auth');
+const port = process.env.PORT || 3000;
 
 const app = express();
 
-// Connect to database
-connectDB();
 
 app.use(express.json());
-
-// Routes
 app.use('/api/auth', authRoutes);
 
-const protectedRoutes = require('./routes/protected');
-app.use('/api/protected', protectedRoutes);
-
-const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
