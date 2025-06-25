37156f580188e4b6f0242a605c07037096239f96
muabiodun99 <Muabiodun101@gmail.com>
2025-06-25 11:02:10 +0100

    feat: restrict application routes to employers for enhanced security

--- routes/application.js
+++ routes/application.js
@@ -1,10 +1,10 @@
 const router= require('express').Router()
 const { application, getApplicationWithJobId, myApplication } = require('../controller/application');
-const { protect } = require('../middleware/authentication');
+const { protect, isEmployer } = require('../middleware/authentication');
 const { uploadApplicationFiles } = require('../utils/multer');
 
-router.post('/apply/:jobId',uploadApplicationFiles,protect, application);
-router.get('/jobs/:jobId/applications',protect, getApplicationWithJobId);
+router.post('/apply/:jobId',uploadApplicationFiles,protect,isEmployer, application);
+router.get('/jobs/:jobId/applications',protect,isEmployer ,getApplicationWithJobId);
 router.get('/my-applications',protect ,myApplication);
 
 module.exports = router
